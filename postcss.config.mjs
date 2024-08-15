97de3767c1931cd3b789fe3358ce587afce54a91
Vladislav Zolotukhin <144418975+vladdyz@users.noreply.github.com>
2024-08-14 21:16:14 -0400

    Add files via upload

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 const config = {
   plugins: {
     tailwindcss,
-    autoprefixer,
   },
 };
 
