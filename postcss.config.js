045e668105c4d1ab6f7e5f3962bd91f11964cd20
Vladislav Zolotukhin <144418975+vladdyz@users.noreply.github.com>
2024-08-14 23:21:14 -0400

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
-    tailwindcss: {},autoprefixer: {},    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    tailwindcss: {}, autoprefixer: {},    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
 }
